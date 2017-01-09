27320be42c122073562c0452923c384d11eeb8dd
techmaster <techmaster@techmasters-iMac-3.local>
2017-01-09 09:57:24 +0700

    Second commit

--- BirdFly/ViewController.swift
+++ BirdFly/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     let bird = UIImageView()
     let imgBirds: [UIImage] = [UIImage(named: "bird0.png")!,
                                UIImage(named: "bird1.png")!,
@@ -17,12 +18,14 @@ class ViewController: UIViewController {
                                UIImage(named: "bird3.png")!,
                                UIImage(named: "bird4.png")!,
                                UIImage(named: "bird5.png")!]
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         drawJungle()
         addBird()
         flyUpAndDown()
+        playMusic()
     }
     
     func drawJungle() {
@@ -30,7 +33,7 @@ class ViewController: UIViewController {
         background.frame = self.view.bounds
         self.view.addSubview(background)
     }
-
+    
     func addBird() {
         bird.frame = CGRect(x: 0.0, y: 0.0, width: 110.0, height: 68.0)
         bird.animationImages = imgBirds
@@ -43,53 +46,33 @@ class ViewController: UIViewController {
     func flyUpAndDown() {
         UIView.animate(withDuration: 3, animations: {
             self.bird.center = CGPoint(x: self.view.bounds.width - self.bird.bounds.width/2, y: self.view.bounds.height - self.bird.bounds.height/2)
-        }, completion: { (true) in
-            self.bird.transform = self.bird.transform.scaledBy(x: -1, y: 1)
-            UIView.animate(withDuration: 2, animations: {
-                self.bird.center = CGPoint(x: self.bird.bounds.width/2, y: self.view.bounds.height - self.bird.bounds.height/2)
             }, completion: { (true) in
-                self.bird.transform = self.bird.transform.scaledBy(x: -1, y: 1).rotated(by: -45 * CGFloat(M_PI/180))
-                UIView.animate(withDuration: 3, animations: {
-                    self.bird.center = CGPoint(x: self.view.bounds.width - self.bird.bounds.width/2, y: self.bird.bounds.height/2)
-                }, completion: { (true) in
-                    self.bird.transform = self.bird.transform.rotated(by: 45 * CGFloat(M_PI/180)).scaledBy(x: -1, y: 1)
-                    UIView.animate(withDuration: 2, animations: { 
-                        self.bird.center = CGPoint(x: self.bird.bounds.width/2, y: self.bird.bounds.height/2)
+                self.bird.transform = self.bird.transform.scaledBy(x: -1, y: 1)
+                UIView.animate(withDuration: 2, animations: {
+                    self.bird.center = CGPoint(x: self.bird.bounds.width/2, y: self.view.bounds.height - self.bird.bounds.height/2)
                     }, completion: { (true) in
-                        self.bird.transform = CGAffineTransform.identity
-                        self.flyUpAndDown()
-                    })
+                        self.bird.transform = self.bird.transform.scaledBy(x: -1, y: 1).rotated(by: -45 * CGFloat(M_PI/180))
+                        UIView.animate(withDuration: 3, animations: {
+                            self.bird.center = CGPoint(x: self.view.bounds.width - self.bird.bounds.width/2, y: self.bird.bounds.height/2)
+                            }, completion: { (true) in
+                                self.bird.transform = self.bird.transform.rotated(by: 45 * CGFloat(M_PI/180)).scaledBy(x: -1, y: 1)
+                                UIView.animate(withDuration: 2, animations: {
+                                    self.bird.center = CGPoint(x: self.bird.bounds.width/2, y: self.bird.bounds.height/2)
+                                    }, completion: { (true) in
+                                        self.bird.transform = CGAffineTransform.identity
+                                        self.flyUpAndDown()
+                                })
+                        })
                 })
-            })
         })
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
+    func playMusic() {
+        let filePath = Bundle.main.path(forResource: "A+ – Chào Mào Mái Hót", ofType: ".mp3")
+        let url = NSURL(fileURLWithPath: filePath!)
+        audioPlayer = try! AVAudioPlayer(contentsOf: url as URL)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
 }
 
